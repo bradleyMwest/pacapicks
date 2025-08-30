a9998d2149fa72cb4f8909267381fdda507052d7
Brad West <bradwest2@gmail.com>
2025-08-30 11:36:30 -0400

    fix package setup

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="pacapicks",
     version="0.1.0",
-    packages=find_packages(where="pacapicks"),
-    package_dir={"": "pacapicks"},
+    packages=find_packages(),
     install_requires=[],  # You can add dependencies here or use requirements.txt
     include_package_data=True,
     description="Alpaca trading and analysis tools",
